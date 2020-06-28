92920c5c009450f77006cd34b469dfad7e6f0130
Ashis-007 <ashisjc@gmail.com>
2020-06-28 14:58:24 +0530

    Fixed #3, added clear history feature

--- src/components/History.js
+++ src/components/History.js
@@ -4,7 +4,20 @@ import Transaction from "./Transaction";
 import "../css/History.css";
 
 const History = () => {
-  const [data] = useContext(Context);
+  const [data, setData] = useContext(Context);
+
+  const clearHistory = () => {
+    if (window.confirm("Do you want to delete your existing history?")) {
+      setData([
+        {
+          amount: 0,
+          text: "", // description
+          date: "",
+        },
+      ]);
+      localStorage.removeItem("data");
+    } else return;
+  };
 
   return (
     <div className="History">
@@ -22,6 +35,11 @@ const History = () => {
                 />
               )
           )}
+        {data.length > 1 && (
+          <button className="btn History__clear" onClick={clearHistory}>
+            Clear History
+          </button>
+        )}
       </div>
     </div>
   );
